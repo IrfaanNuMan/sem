f1bb358ebd3c30bf349074891fa3acacd9b82590
IrfaanNuMan <irfaan_numan@hotmail.com>
2021-03-19 06:40:09 -0600

    reduced retries to 20 and thread sleep to 0

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -19,14 +19,14 @@ public class App
 
         // Connection to the database
         Connection con = null;
-        int retries = 100;
+        int retries = 20;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(0);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world.SQL/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
