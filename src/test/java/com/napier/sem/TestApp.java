10e169163726d754704ecceb07fb2f1a434f2bd4
IrfaanNuMan <irfaan_numan@hotmail.com>
2021-04-18 14:45:30 -0600

    unit added for all capital cities in the world desc

--- src/test/java/com/napier/sem/TestApp.java
+++ src/test/java/com/napier/sem/TestApp.java
@@ -10,17 +10,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestApp {
+    static App app;
+    private Object ArrayList;
 
-
-    @Test
-    void unitTest()
-    {
-        assertEquals(5, 5);
-    }
-
-   /* @BeforeAll
+    @BeforeAll
     static void init()
     {
         app = new App();
-    }*/
-}
+    }
+    @Test
+    void printAllCapCitiesInWorldDescTestNull() {
+        app.printAllCapCitiesInWorldDesc(null);
+    }
+    @Test
+    void printAllCapCitiesInWorldDescTestEmpty() {
+        app.printAllCapCitiesInWorldDesc(null);
+    }
+    @Test
+    void printAllCapCitiesInWorldDescTestConNull() {
+        ArrayList<City> cy5 = new ArrayList<City>();
+        cy5.add(null);
+        app.printAllCapCitiesInWorldDesc(null);
+    }
+    @Test
+    void printAllCapCitiesInWorldDesc() {
+        ArrayList<City> cy5 = new ArrayList<City>();
+        City city = new City();
+        city.ID = 2331;
+       /* city.Name = Seoul;
+        city.CountryCode = KOR;
+        city.District = Seoul;*/
+        city.Population = 9981619;
+        cy5.add(city);
+        app.printAllCapCitiesInWorldDesc(cy5);
+
+    }
+    }
+
+
+
+
