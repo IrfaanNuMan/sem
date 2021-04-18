f299869f15465547fc979780d3706dd1b23ccabc
Val-01 <aidianeth10@gmail.com>
2021-04-18 13:56:53 -0600

    unit test being added

---
+++ src/test/java/com/napier/sem/TestApp.java
@@ -0,0 +1,26 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestApp {
+
+
+    @Test
+    void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+
+   /* @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }*/
+}
