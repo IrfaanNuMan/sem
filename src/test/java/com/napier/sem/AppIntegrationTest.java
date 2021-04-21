8b044880024a78d468c220895fc5264107b91477
IrfaanNuMan <irfaan_numan@hotmail.com>
2021-04-20 21:03:12 -0600

    updated AppIntegrationTest

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.napier.sem;
+
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AppIntegrationTest {
